90c225c24b84160afdc5fc35d0b569d3d99d093c
Ashelle Cancio <vermillion707@gmail.com>
2022-11-09 23:03:06 +0000

    Set up hash routing

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import Home from './pages/home';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', event => {
+      const route = parseRoute(window.location.hash);
+      this.setState({
+        route
+      });
+    });
+  }
+
   render() {
-    return <Home />;
+    const { route } = this.state;
+    if (route.path === '') {
+      return <Home />;
+    }
+    if (route.path === 'bye') {
+      return 'Bye!';
+    }
   }
 }
